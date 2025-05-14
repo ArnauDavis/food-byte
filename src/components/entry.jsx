48f8642ce5bb43e9e10104247ec0c5126cc3a4e2
Arnau Davis <arnau.davis@outlook.com>
2025-05-14 15:46:45 -0400

    fixed some button and heart alignment

--- src/components/entry.jsx
+++ src/components/entry.jsx
@@ -1,4 +1,5 @@
 function Entry({props}) {
+//add colors here for tailwind to recognize them
     // bg-slate-200 bg-gray-200 bg-zinc-200 bg-neutral-200 bg-stone-200 bg-red-200 bg-orange-200 bg-amber-200 bg-yellow-200 bg-lime-200 bg-green-200 bg-emerald-200 bg-teal-200 bg-cyan-200 bg-sky-200 bg-blue-200 bg-indigo-200 bg-violet-200 bg-purple-200 bg-fuchsia-200 bg-pink-200 bg-rose-200
 // from-slate-200 from-gray-200 from-zinc-200 from-neutral-200 from-stone-200 from-red-200 from-orange-200 from-amber-200 from-yellow-200 from-lime-200 from-green-200 from-emerald-200 from-teal-200 from-cyan-200 from-sky-200 from-blue-200 from-indigo-200 from-violet-200 from-purple-200 from-fuchsia-200 from-pink-200 from-rose-200
 // to-slate-600 to-gray-600 to-zinc-600 to-neutral-600 to-stone-600 to-red-600 to-orange-600 to-amber-600 to-yellow-600 to-lime-600 to-green-600 to-emerald-600 to-teal-600 to-cyan-600 to-sky-600 to-blue-600 to-indigo-600 to-violet-600 to-purple-600 to-fuchsia-600 to-pink-600 to-rose-600
@@ -18,10 +19,10 @@ function Entry({props}) {
                 <p className="font-light mb-4">
                     {props.intro}
                 </p>
-                <section className="flex gap-5 content-stretch justify-center mb-6">
-                    <button className={`flex-grow font-semibold rounded-xl text-white bg-gradient-to-b from-${props.backgroundColor}-200 to-${props.backgroundColor}-600 uppercase pt-2 pb-2 px-10 ml-10 hover:bg-gradient-to-br md:flex-none`}>
+                <section className="flex gap-5 content-stretch justify-center items-center mb-6">
+                    <button className={`flex-grow font-semibold rounded-xl text-white bg-gradient-to-b from-${props.backgroundColor}-200 to-${props.backgroundColor}-600 uppercase pt-2 pb-2 px-10  hover:bg-gradient-to-br md:flex-none`}>
                       add to cart</button>
-                    <svg class={`size-10 text-${props.backgroundColor}-600 mr-10 transition-transform hover:scale-125 duration-300`}
+                    <svg class={`size-10 text-${props.backgroundColor}-600 transition-transform hover:scale-125 duration-300`}
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                     </svg>
